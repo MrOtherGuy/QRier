245b0fd3903935340d6c7d5239badddf074d5dc1
MrOtherGuy <jastekken@outlook.com>
2022-05-23 17:41:41 +0300

    MV3: Make some required changes to options page

--- options/options.js
+++ options/options.js
@@ -1,6 +1,14 @@
+'use strict';
 function saveOptions(e) {
-	var newMenus = {"onLink":document.querySelector("#onLink").checked,"onUrl":document.querySelector("#onUrl").checked,"onSelection":document.querySelector("#onSelect").checked};
-	var newOutput = document.getElementById("outputType").elements["output"].value;
+	let newOutput = document.getElementById("outputType").elements["output"].value === "popup" ? "popup" : "content";
+  
+  let newMenus = {
+    "inContent": newOutput === "content",
+    "onLink": document.querySelector("#onLink").checked,
+    "onUrl": document.querySelector("#onUrl").checked,
+    "onSelection": document.querySelector("#onSelect").checked
+  };
+	
   browser.storage.local.set({
     mask: parseInt(document.querySelector("#maskSelect").value),
 		ecc: parseInt(document.querySelector("#eccSelect").value),
@@ -16,15 +24,15 @@ function saveOptions(e) {
 }
 
 function notifyBackground(output){
-	var sending = browser.runtime.sendMessage({
-		value: output
-	});
-	sending.then(handleResponse,handleError);
+	browser.runtime.sendMessage({
+		outputMode: output
+	})
+	.then(handleResponse,handleError);
 }
 
 function handleResponse(message) {
   console.log(`Resolution:  ${message.response}`);
-	feedback(false,"OK");
+	feedback(false,"Changes saved");
 }
 
 function handleError(error) {
@@ -50,52 +58,69 @@ function onError() {
 
 function feedback(err,str){
 	var elem = document.getElementById("feedback");
-	var color = err ? "red":"green";
+	var color = err ? "pink":"lightgreen";
 	var fdb = err ? "Error while saving options, see browser console " + str : "OK";
 	elem.textContent = fdb;
-	elem.style.color = color;
+	elem.style.backgroundColor = color;
+  setTimeout(()=>{ elem.textContent = " "; elem.style.backgroundColor = "transparent"},2000)
 	return 0
 }
 
 function updateMenus(newMenus){
-	if (menuStates.onLink != newMenus.onLink){
-		if(menuStates.onLink){
-			browser.menus.remove("openMenuLink").then(onRemoved,onError);
+  
+  const addSuffix = newMenus.inContent ? "In-content" : "In-popup";
+  const removeSuffix = newMenus.inContent ? "In-popup" : "In-content";
+  
+  const inContentStateChanged = newMenus.inContent != menuStates.inContent;
+  
+  if(inContentStateChanged){
+    browser.menus.remove("openMenuLink"+removeSuffix);
+    browser.menus.remove("openMenuUrl"+removeSuffix);
+    browser.menus.remove("openMenuSelection"+removeSuffix);
+    menuStates.inContent != menuStates.inContent;
+  }
+  
+	if(inContentStateChanged || (menuStates.onLink != newMenus.onLink)){
+		if(!newMenus.onLink){
+			browser.menus.remove("openMenuLink"+removeSuffix)
+      .then(onRemoved,onError);
 		}else{
 			browser.menus.create({
-				id: "openMenuLink",
+				id: "openMenuLink"+addSuffix,
 				title: "QRier link",
 				contexts: ["link"]
 				}, onCreated);
 		}
-		menuStates.onLink = !menuStates.onLink;
+		menuStates.onLink = newMenus.onLink;
 	}
 	
-	if (menuStates.onUrl != newMenus.onUrl){
+	if(inContentStateChanged || (menuStates.onUrl != newMenus.onUrl)){
 
-		if(menuStates.onUrl){
-			browser.menus.remove("openMenuUrl").then(onRemoved,onError);
+		if(!newMenus.onUrl){
+			browser.menus.remove("openMenuUrl"+removeSuffix)
+      .then(onRemoved,onError);
 		}else{
 			browser.menus.create({
-				id: "openMenuUrl",
+				id: "openMenuUrl"+addSuffix,
 				title: "QRier url",
 				contexts: ["page"]
 				}, onCreated);
 		}
-		menuStates.onUrl = !menuStates.onUrl;
+		menuStates.onUrl = newMenus.onUrl;
 	}
-	if (menuStates.onSelection != newMenus.onSelection){
+	if(inContentStateChanged || (menuStates.onSelection != newMenus.onSelection)){
 
-		if(menuStates.onSelection){
-			browser.menus.remove("openMenuSelection").then(onRemoved,onError);
+		if(!newMenus.onSelection){
+			browser.menus.remove("openMenuSelection"+removeSuffix)
+      .then(onRemoved,onError);
 		}else{
 			browser.menus.create({
-				id: "openMenuSelection",
+				id: "openMenuSelection"+addSuffix,
 				title: "QRier selection",
 				contexts: ["selection"]
 				}, onCreated);
 		}
-		menuStates.onSelection = !menuStates.onSelection;
+		menuStates.onSelection = newMenus.onSelection;
 	}
 }
 
@@ -116,10 +141,16 @@ function restoreOptions() {
 		menuStates.onUrl = res.showUrl;
 		document.querySelector("#onSelect").checked = res.showSelection;
 		menuStates.onSelection = res.showSelection;
-		document.getElementById(res.inContent).checked = true;
+		document.getElementById(res.inContent+"-radio").checked = true;
+    menuStates.inContent = res.inContent === "content";
   });
 }
-var menuStates = {"onLink":false,"onUrl":false,"onSelection":false};
+const menuStates = {
+  "onLink": false,
+  "onUrl": false,
+  "onSelection": false,
+  "inContent": false
+};
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.getElementById("saveButton").addEventListener("click", saveOptions,false);
