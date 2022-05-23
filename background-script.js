dc93cd6f352c595d804893233a28b315abc91cc8
MrOtherGuy <jastekken@outlook.com>
2022-05-23 17:39:12 +0300

    MV3: rewrite background-script.js to non-persistent event page

--- background-script.js
+++ background-script.js
@@ -1,199 +1,215 @@
-browser.runtime.onInstalled.addListener(setDefault);
-browser.runtime.onMessage.addListener(handleMessage);
-function setDefault() {
+// Install listener
+browser.runtime.onInstalled.addListener(function() {
+  setDefaultOptions()
+  .then(setDefaultMenus)
+  .catch(e => { console.error(e) })
+});
 
-  var gettingItem = browser.storage.local.get(['mask','ecc','scale','showLink','showUrl','showSelection','inContent']);
-  gettingItem.then((res) => {
-    var mask = res.mask || 9;
-		var ecc = res.ecc || 3;
-		var scale = res.scale || 6;
-		var showLink = res.showLink || false;
-		var showUrl = res.showUrl || false;
-		var showSelection = res.showSelection || false;
-		var showInContent = res.inContent || "popup";
-		console.log("Options set to: "+Object.entries(res));
-		browser.storage.local.set({
-    mask: mask,
-		ecc: ecc,
-		scale: scale,
-		showLink: showLink,
-		showUrl: showUrl,
-		showSelection: showSelection,
-		inContent: showInContent
-		});
+browser.omnibox.onInputEntered.addListener((input) => {
+  
+  // Use tab url when no input
+  // ActiveTab permission doesn't work on omnibox trigger currently
+  // If no input is given we should default to current url
+  let freePageURI = browser.runtime.getURL("pages/QRierFreepage.html");
+  
+  browser.storage.local.get(['mask','ecc'])
+  .then((options) => {
+    let mask = options.mask;
+    let ecc = [null,"L","M","Q","H"][options.ecc];
+    browser.tabs.update({
+      url: `${freePageURI}?data=${fixedEncodeURIComponent(input)}&mask=${mask}&ecc=${ecc}`
+    });
   });
-}
+});
 
+browser.runtime.onMessage.addListener(handleMessage);
 
-function onCreated() {
-  if (browser.runtime.lastError) {
-    console.log(`Error: ${browser.runtime.lastError}`);
-  } else {
-    console.log("Item created successfully");
+browser.menus.onClicked.addListener((menus,tab) => {
+  if(menus.menuItemId === "openEditor"){
+    browser.tabs.create({
+      "url": browser.runtime.getURL("pages/QRierFreepage.html")
+    });
+  }else{
+    const type = menus.menuItemId.match(/In-(popup|content)/)?.[1]
+    switch (type){
+      case "content":
+        openMenuActionInContent(menus,tab);
+        break;
+      case "popup":
+        openMenuActionInPanel(menus);
+        break;
+      default:
+        console.warn("unimplemented menu action: "+type)
+    }
   }
+})
+// Set listener for browserAction button
+browser.action.onClicked.addListener((tab) => {
+  openBrowserActionInContent(tab)
+});
+// This effectively disables browserAction listener when needed.
+// We could check  inContent == "popup", but there might be a 
+// chance that key isn't set to anything on new install, and
+// "popup" is the default setting
+browser.storage.local.get("inContent")
+.then(some => {
+  if(some.inContent != "content"){
+    browser.action.setPopup({ popup: "../popup/QRier.html" })
+  }
+});
+
+async function setDefaultOptions() {
+  let res = await browser.storage.local.get(['mask','ecc','scale','showLink','showUrl','showSelection','inContent']);
+  let mask = res.mask || 9;
+  let ecc = res.ecc || 3;
+  let scale = res.scale || 6;
+  let showLink = res.showLink || false;
+  let showUrl = res.showUrl || false;
+  let showSelection = res.showSelection || false;
+  let showInContent = res.inContent || "popup";
+  let values = {
+    mask: mask,
+    ecc: ecc,
+    scale: scale,
+    showLink: showLink,
+    showUrl: showUrl,
+    showSelection: showSelection,
+    inContent: showInContent
+  };
+  await browser.storage.local.set(values);
+  if(showInContent === "popup"){
+    browser.action.setPopup({ popup: "../popup/QRier.html" })
+  }
+  return { ok: true, values: values }
 }
 
-/*
-Called when the item has been removed.
-We'll just log success here.
-*/
-function onRemoved() {
-  console.log("Item removed successfully");
-}
-
-/*
-Called when there was an error.
-We'll just log the error here.
-*/
-function onError(error) {
-  console.log(`Error: ${error}`);
-}
-
-function setMenus(menus){
+function setDefaultMenus( settings ){
 	
 	if(!browser.menus){
 		return
 	}
-	
+  if(!settings.ok){
+    console.error("default settings failed somehow");
+    console.log(settings);
+  }
+  const menus = settings.values;
+  
 	// Create a context menu entry for browserAction to open editor
 	browser.menus.create({
 		id: "openEditor",
 		title: "QRier Editor",
-		contexts: ["browser_action"]
-	}, onCreated);
-	
+		contexts: ["action"]
+	});
+  const suffix = "In-" + menus.inContent;
 	if(menus.showLink){
 		browser.menus.create({
-			id: "openMenuLink",
+			id: "openMenuLink"+suffix,
 			title: "QRier link",
 			contexts: ["link"]
-		}, onCreated);
+		});
 	}
 	if(menus.showSelection){
 		browser.menus.create({
-			id: "openMenuSelection",
+			id: "openMenuSelection"+suffix,
 			title: "QRier selection",
 			contexts: ["selection"]
-		}, onCreated);
+		});
 	}
 	if(menus.showUrl){
 		browser.menus.create({
-			id: "openMenuUrl",
+			id: "openMenuUrl"+suffix,
 			title: "QRier url",
 			contexts: ["page"]
-		}, onCreated);
+		});
 	}
-	browser.menus.onClicked.addListener((menus, tab) => {
-		if (menus.menuItemId === "openEditor"){
-			browser.tabs.create({url:freePageURI});
-		}else{
-			var uri = "";
-			if(info.outputType === "popup"){
-				uri += popupBaseURI + "?";
-			}
-			switch(menus.menuItemId){
-				case "openMenuLink":
-					uri += menus.linkUrl; 
-					break;
-				case "openMenuSelection":
-					uri += menus.selectionText;
-					break;
-				case "openMenuUrl":
-					uri += menus.pageUrl;
-					break
-			}
-			switch(info.outputType){
-				case "popup":
-					browser.browserAction.setPopup({popup:uri});
-					browser.browserAction.openPopup();
-					break;
-				case "content":
-					handleInContent(uri);
-					break;
-				default:
-					console.log("unexpected output type: " + info.outputType);
-			}
-		}
-	});
+}
+
+function openBrowserActionInPanel(tab){
+  browser.action.setPopup({popup:"../popup/QRier.html?"+tab.url});
+  return browser.action.openPopup()
+}
+
+function openBrowserActionInContent(tab){
+  if(!/^http/.test(tab.url)){
+    openBrowserActionInPanel(tab)
+    .then(()=>browser.action.setPopup({popup:null}));
+    return
+  }
+  browser.scripting.executeScript({
+    files: ["inContentScript.js"],
+    target: {tabId: tab.id}
+  })
+  .then((e) => {
+    setTimeout(()=>{
+      browser.tabs.sendMessage(
+        tab.id,
+        { menudata: tab.url }
+      )
+    },60) // time for connection to be made to content script
+  })
+  .catch(console.error)
+}
+
+function openMenuActionInPanel(menus){
+  let payloadText = "../popup/QRier.html?"+getStringFromTriggerAction(menus);
+  browser.action.setPopup({popup:payloadText});
+  return browser.action.openPopup()
+}
+
+function openMenuActionInContent(menus, tab){
+  if(!/^http/.test(tab.url)){
+    openMenuActionInPanel(menus)
+    .then(()=>browser.action.setPopup({popup:null}));
+    return
+  }
+  browser.scripting.executeScript({
+    files: ["inContentScript.js"],
+    target: {tabId: tab.id}
+  })
+  .then((e) => {
+    setTimeout(()=>{
+      browser.tabs.sendMessage(
+        tab.id,
+        { menudata: getStringFromTriggerAction(menus) }
+      )
+    },60) // time for connection to be made to content script
+  })
+  .catch(console.error);
+}
+
+function getStringFromTriggerAction(menus){
+  switch(menus.menuItemId){
+    case "openMenuLinkIn-content":
+    case "openMenuLinkIn-popup":
+      return menus.linkUrl; 
+    case "openMenuSelectionIn-content":
+    case "openMenuSelectionIn-popup":
+      return menus.selectionText;
+    case "openMenuUrlIn-content":
+    case "openMenuUrlIn-popup":
+      return menus.pageUrl;
+  }
+  return ""
 }
 
 function handleMessage(request, sender, sendResponse) {
-  console.log("Should set type to: " +
-    request.value.inContent);
-	setOutput(request.value);
-  sendResponse({response: "success: " + info.outputType});
+  if(sender.id != browser.runtime.id){
+    return Promise.resolve({response: null})
+  }
+  if(request?.outputMode.inContent === "content"){
+    browser.action.setPopup({popup: null})
+    return Promise.resolve({response:"success"});
+  }
+  if(request?.outputMode.inContent === "popup"){
+    browser.action.setPopup({ popup: "../popup/QRier.html" })
+    return Promise.resolve({response:"success"})
+  }
+
 }
 
-
-function setOutput(output){
-	info.outputType = output.inContent;
-	// Can't set popup to "" on Android
-	// This will disable in-content functionality on Android
-	if (info.outputType === "content" && info.os != "android"){
-		browser.browserAction.setPopup({popup:""});
-		if(!QRierObject){
-			QRierObject = new QRier_Gen();
-		}
-	}else{
-		// popup:null should set the default popup but that doesn't work on Android
-		browser.browserAction.setPopup({popup:popupBaseURI});
-		QRierObject = null;
-	}
-}
-	
-function setOS(os){
-	info.os = os.os;
-}
 // Extends encodeURIComponent() to include !'()*
 function fixedEncodeURIComponent(str) {
   return encodeURIComponent(str).replace(/[!'()*]/g, function(c) {
     return '%' + c.charCodeAt(0).toString(16);
   });
 }
-	
-	function setup(){
-		var gettingOutput = browser.storage.local.get(['inContent']);
-		var gettingMenus = browser.storage.local.get(['showLink','showUrl','showSelection']);
-		var gettingOS = browser.runtime.getPlatformInfo();
-		
-		Promise.all([gettingOutput,gettingMenus,gettingOS]).then(setup => {
-			setOS(setup[2]);
-			setOutput(setup[0]);
-			setMenus(setup[1]);
-
-		});
-	}
-	
-	var info = { os: null, outputType: "popup" }
-	var QRierObject = null;
-	const freePageURI = browser.runtime.getURL("pages/QRierFreepage.html");
-	const popupBaseURI = "../popup/QRier.html";
-	setup();
-	
-	// Setup omnibox if supported
-	if(browser.omnibox){
-		browser.omnibox.onInputEntered.addListener((input) => {
-		
-		// Use tab url when no input
-		// ActiveTab permission doesn't work on omnibox trigger currently
-		// If no input is given we should default to current url
-		if(false){
-			browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
-				browser.tabs.update({url:freePageURI + "?" + tabs[0].url});
-			});
-		}else{
-			browser.storage.local.get(['mask','ecc']).then((options) => {
-				var mask = options.mask;
-				var ecc = [null,"L","M","Q","H"][options.ecc];
-				browser.tabs.update({url:freePageURI + "?data=" + fixedEncodeURIComponent(input) + "&mask=" + mask + "&ecc=" + ecc});
-			});
-		}
-	});
-	}else{
-		console.log("no omnibox support");
-	}
-	// This will not fire if there is a popup specified
-
-	browser.browserAction.onClicked.addListener((tab) => {
-			handleInContent(tab.url)
-		});
