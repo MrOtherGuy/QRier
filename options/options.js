22ebe02b46709fb3ce2f7454b7a4be19738f165d
MrOtherGuy <jastekken@outlook.com>
2022-05-23 17:52:01 +0300

    convert tabs to spaces to keep in line with other files

--- options/options.js
+++ options/options.js
@@ -1,6 +1,6 @@
 'use strict';
 function saveOptions(e) {
-	let newOutput = document.getElementById("outputType").elements["output"].value === "popup" ? "popup" : "content";
+  let newOutput = document.getElementById("outputType").elements["output"].value === "popup" ? "popup" : "content";
   
   let newMenus = {
     "inContent": newOutput === "content",
@@ -8,36 +8,36 @@ function saveOptions(e) {
     "onUrl": document.querySelector("#onUrl").checked,
     "onSelection": document.querySelector("#onSelect").checked
   };
-	
+  
   browser.storage.local.set({
     mask: parseInt(document.querySelector("#maskSelect").value),
-		ecc: parseInt(document.querySelector("#eccSelect").value),
-		scale: parseInt(document.querySelector("#scaleSelect").value),
-		showLink: newMenus.onLink,
-		showUrl: newMenus.onUrl,
-		showSelection: newMenus.onSelection,
-		inContent: newOutput
+    ecc: parseInt(document.querySelector("#eccSelect").value),
+    scale: parseInt(document.querySelector("#scaleSelect").value),
+    showLink: newMenus.onLink,
+    showUrl: newMenus.onUrl,
+    showSelection: newMenus.onSelection,
+    inContent: newOutput
   });
   e.preventDefault();
-	updateMenus(newMenus);
-	notifyBackground({inContent:newOutput});
+  updateMenus(newMenus);
+  notifyBackground({inContent:newOutput});
 }
 
 function notifyBackground(output){
-	browser.runtime.sendMessage({
-		outputMode: output
-	})
-	.then(handleResponse,handleError);
+  browser.runtime.sendMessage({
+    outputMode: output
+  })
+  .then(handleResponse,handleError);
 }
 
 function handleResponse(message) {
   console.log(`Resolution:  ${message.response}`);
-	feedback(false,"Changes saved");
+  feedback(false,"Changes saved");
 }
 
 function handleError(error) {
   console.log(`Error: ${error}`);
-	feedback(true,"");
+  feedback(true,"");
 }
 
 function onCreated() {
@@ -57,13 +57,13 @@ function onError() {
 }
 
 function feedback(err,str){
-	var elem = document.getElementById("feedback");
-	var color = err ? "pink":"lightgreen";
-	var fdb = err ? "Error while saving options, see browser console " + str : "OK";
-	elem.textContent = fdb;
-	elem.style.backgroundColor = color;
+  var elem = document.getElementById("feedback");
+  var color = err ? "pink":"lightgreen";
+  var fdb = err ? "Error while saving options, see browser console " + str : "OK";
+  elem.textContent = fdb;
+  elem.style.backgroundColor = color;
   setTimeout(()=>{ elem.textContent = " "; elem.style.backgroundColor = "transparent"},2000)
-	return 0
+  return 0
 }
 
 function updateMenus(newMenus){
@@ -80,68 +80,64 @@ function updateMenus(newMenus){
     menuStates.inContent != menuStates.inContent;
   }
   
-	if(inContentStateChanged || (menuStates.onLink != newMenus.onLink)){
-		if(!newMenus.onLink){
-			browser.menus.remove("openMenuLink"+removeSuffix)
+  if(inContentStateChanged || (menuStates.onLink != newMenus.onLink)){
+    if(!newMenus.onLink){
+      browser.menus.remove("openMenuLink"+removeSuffix)
       .then(onRemoved,onError);
-		}else{
-			browser.menus.create({
-				id: "openMenuLink"+addSuffix,
-				title: "QRier link",
-				contexts: ["link"]
-				}, onCreated);
-		}
-		menuStates.onLink = newMenus.onLink;
-	}
-	
-	if(inContentStateChanged || (menuStates.onUrl != newMenus.onUrl)){
+    }else{
+      browser.menus.create({
+        id: "openMenuLink"+addSuffix,
+        title: "QRier link",
+        contexts: ["link"]
+        }, onCreated);
+    }
+    menuStates.onLink = newMenus.onLink;
+  }
+  
+  if(inContentStateChanged || (menuStates.onUrl != newMenus.onUrl)){
 
-		if(!newMenus.onUrl){
-			browser.menus.remove("openMenuUrl"+removeSuffix)
+    if(!newMenus.onUrl){
+      browser.menus.remove("openMenuUrl"+removeSuffix)
       .then(onRemoved,onError);
-		}else{
-			browser.menus.create({
-				id: "openMenuUrl"+addSuffix,
-				title: "QRier url",
-				contexts: ["page"]
-				}, onCreated);
-		}
-		menuStates.onUrl = newMenus.onUrl;
-	}
-	if(inContentStateChanged || (menuStates.onSelection != newMenus.onSelection)){
+    }else{
+      browser.menus.create({
+        id: "openMenuUrl"+addSuffix,
+        title: "QRier url",
+        contexts: ["page"]
+        }, onCreated);
+    }
+    menuStates.onUrl = newMenus.onUrl;
+  }
+  if(inContentStateChanged || (menuStates.onSelection != newMenus.onSelection)){
 
-		if(!newMenus.onSelection){
-			browser.menus.remove("openMenuSelection"+removeSuffix)
+    if(!newMenus.onSelection){
+      browser.menus.remove("openMenuSelection"+removeSuffix)
       .then(onRemoved,onError);
-		}else{
-			browser.menus.create({
-				id: "openMenuSelection"+addSuffix,
-				title: "QRier selection",
-				contexts: ["selection"]
-				}, onCreated);
-		}
-		menuStates.onSelection = newMenus.onSelection;
-	}
+    }else{
+      browser.menus.create({
+        id: "openMenuSelection"+addSuffix,
+        title: "QRier selection",
+        contexts: ["selection"]
+        }, onCreated);
+    }
+    menuStates.onSelection = newMenus.onSelection;
+  }
 }
 
 function restoreOptions() {
 
-  var gettingItem = browser.storage.local.get(['mask','ecc','scale','showLink','showUrl','showSelection','inContent']);
-  gettingItem.then((res) => {
-
-		document.querySelector("#maskSelect").value = res.mask.toString();
-
-		document.querySelector("#eccSelect").value = res.ecc.toString();
-
-		document.querySelector("#scaleSelect").value = res.scale.toString();
-
-		document.querySelector("#onLink").checked = res.showLink;
-		menuStates.onLink = res.showLink;
-		document.querySelector("#onUrl").checked = res.showUrl;
-		menuStates.onUrl = res.showUrl;
-		document.querySelector("#onSelect").checked = res.showSelection;
-		menuStates.onSelection = res.showSelection;
-		document.getElementById(res.inContent+"-radio").checked = true;
+  browser.storage.local.get(['mask','ecc','scale','showLink','showUrl','showSelection','inContent'])
+  .then((res) => {
+    document.querySelector("#maskSelect").value = res.mask.toString();
+    document.querySelector("#eccSelect").value = res.ecc.toString();
+    document.querySelector("#scaleSelect").value = res.scale.toString();
+    document.querySelector("#onLink").checked = res.showLink;
+    menuStates.onLink = res.showLink;
+    document.querySelector("#onUrl").checked = res.showUrl;
+    menuStates.onUrl = res.showUrl;
+    document.querySelector("#onSelect").checked = res.showSelection;
+    menuStates.onSelection = res.showSelection;
+    document.getElementById(res.inContent+"-radio").checked = true;
     menuStates.inContent = res.inContent === "content";
   });
 }
